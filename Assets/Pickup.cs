5251821a5af0f941609c1b7475b641ba9ae8a69c
Zouz <zouz.astek@gmail.com>
2021-04-17 22:45:50 +0200

    Push for unity

---
+++ Assets/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    protected void awake()
+    {
+        this._rigidbody = this.GetComponent<Rigidbody>();
+        this._animator = this.GetComponentInChildren<Animator>();
+    }
+
+    void Start()
+    {
+        this._animator?.SetBool("Waiting", true);
+    }
+
+    void Update()
+    {
+        if (this._animator != null)
+        {
+            Debug.Log("I have no animator");
+        }
+    }
+}
